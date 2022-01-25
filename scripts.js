8e763c251682ee076cdb17c6ca6fedc938ef6b07
Wellington Nogueira <wellingtoncw7@gmail.com>
2022-01-24 21:14:22 -0300

    formatação da moeda

--- scripts.js
+++ scripts.js
@@ -45,7 +45,7 @@ const Transaction = {
 }
 
 const DOM = {
-    transactionsContainer: document.querySelector('data-table tbody'),
+    transactionsContainer: document.querySelector('#data-table tbody'),
 
     addTransaction(transaction, index) {
         console.log(transaction)
@@ -56,10 +56,14 @@ const DOM = {
     },
 
     innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = `
         <tr>
             <td class="description">${transaction.description}</td>
-            <td class="expense">${transaction.expense}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
             <img src="./assets/minus.svg" alt="Remover transação">
@@ -70,4 +74,23 @@ const DOM = {
     }
 }
 
-DOM.addTransaction(transactions[0])
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, '')
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        })
+        return signal + value
+    }
+}
+
+// para adicionar as transações que estão dentro do Array na página
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
