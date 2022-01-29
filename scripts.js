d8d5a8a12ce23c38d8e0f99eb727086c8e368738
Wellington Nogueira <wellingtoncw7@gmail.com>
2022-01-29 11:15:23 -0300

    Update scripts.js
    bug corrigido

--- scripts.js
+++ scripts.js
@@ -119,8 +119,8 @@ const DOM = {
 
 const Utils = {
     formatAmount(value) {
-        value = Number(value.replace(/\,\./g, "")) * 100
-        return value;
+        value = value * 100
+        return Math.round(value);
     },
 
     formatDate(date) {
